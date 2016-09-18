f2d389b03a405d5513362b6f58596de98ed6594f
ZhaoZhenTang <tangzhaozhen8@gmail.com>
2016-09-18 08:45:44 +0000

    basic login half 8.2.3

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,6 @@
 module UsersHelper
+    
+    #Create specific icon for user
     def gravatar_for(user)
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   
 
+  get 'sessions/new'
+
   root 'static_pages#home'
   
 
@@ -10,6 +12,11 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
   post '/signup',  to: 'users#create'
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
 
   resources :users
  
